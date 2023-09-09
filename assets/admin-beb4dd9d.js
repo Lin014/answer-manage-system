114c69c288125a14861eea7c373ef7cfcd59a261
Amelia <ciling@gmail.com>
2023-09-09 20:17:53 +0800

    Updates

--- assets/admin-beb4dd9d.js
+++
@@ -1,1 +0,0 @@
-import"./main-6bc30d85.js";const t="Admin";document.querySelectorAll(".nav-link").forEach(e=>{e.getAttribute("data-page")===t?e.classList.add("active"):e.classList.remove("active")});
---
+++ assets/admin-da4b6571.js
@@ -0,0 +1,1 @@
+import"./main-7d92d73b.js";const t="Admin";document.querySelectorAll(".nav-link").forEach(e=>{e.getAttribute("data-page")===t?e.classList.add("active"):e.classList.remove("active")});
